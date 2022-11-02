0b097eb9b9fd08dc4f24bfdfa29f7361980eb4c2
osamaashraf6 <osamaashraf668@gmail.com>
2022-11-02 21:02:16 +0200

    Add the js file for loop on the data which created by array of object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
+// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
+// All required elements
+const contents = document.querySelector('.contents');
+// define data
+const todos = [
+  {
+    id: 1,
+    completed: false,
+    desc: 'Task completed1',
+  },
+  {
+    id: 2,
+    completed: false,
+    desc: 'Task completed2',
+  },
+  {
+    id: 3,
+    completed: false,
+    desc: 'Task completed3',
+  },
+  {
+    id: 4,
+    completed: false,
+    desc: 'Task completed4',
+  },
+  {
+    id: 5,
+    completed: false,
+    desc: 'Task completed5',
+  },
+];
+// Show functionality
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+// eslint-disable-next-line no-restricted-syntax
+for (const todo of todos) {
+  contents.innerHTML += `
+    <div class="content">
+    <input type="checkbox" />
+    <p contenteditable="true">${todo.desc}</p>
+    <span><i class="far fa-dot-circle"></i></span>
+  </div>
+    `;
 }
-
-document.body.appendChild(component());
